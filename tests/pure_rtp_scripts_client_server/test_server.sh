8f0998c51f2c47f3d0146f14563700318572761d
dmitry <dmitry@dmitry-lenovoW530.cablelns.012.net.il>
2013-01-17 00:18:50 +0200

    adding scripts to simulate fast rtp transfer between server and client

---
+++ tests/pure_rtp_scripts_client_server/test_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+AV=/dev/video0
+VIDEO_UDP_PORT=9978
+width=640
+height=480
+gst-launch-0.10 --gst-debug-level=0 v4l2src device=$AV ! video/x-raw-yuv,width=$width,height=$height ! \
+videorate ! ffmpegcolorspace ! video/x-raw-yuv,width=$width,height=$height ! \
+x264enc tune=zerolatency byte-stream=true bitrate=3000 ! rtph264pay ! \
+udpsink host=127.0.0.1 port=$VIDEO_UDP_PORT sync=false
