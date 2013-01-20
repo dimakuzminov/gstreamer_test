fd4b569653d791e0a49d31704aeb4845bf441852
dmitry <dmitry@dmitry-ThinkPad-W530.(none)>
2013-01-20 16:35:20 +0200

    Adding more packages

--- install_essential_ubuntu.sh
+++ install_essential_ubuntu.sh
@@ -4,3 +4,12 @@ sudo apt-get install libx264*
 sudo apt-get install libgst*
 sudo apt-get install gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly-*
 sudo apt-get install gstreamer0.10-ffmpeg
+
+
+sudo apt-get install libgtk*
+sudo apt-get install libz-dev
+sudo apt-get install liblzo2-dev
+sudo apt-get install libSDL*
+sudo apt-get install libboost-thread1.46-dev
+
+sudo apt-get install vlc
