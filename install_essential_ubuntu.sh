606f1c10f25b431adcbee886d065c9dbbd205ecc
dmitry <dmitry@dmitry-ThinkPad-W530.(none)>
2013-01-20 16:23:13 +0200

    Fix installation bug

--- install_essential_ubuntu.sh
+++ install_essential_ubuntu.sh
@@ -2,6 +2,5 @@ sudo apt-get update
 sudo apt-get install libgstreamer*
 sudo apt-get install libx264*
 sudo apt-get install libgst*
-sudo apt-get install gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly-multiverse
 sudo apt-get install gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly-*
 sudo apt-get install gstreamer0.10-ffmpeg
