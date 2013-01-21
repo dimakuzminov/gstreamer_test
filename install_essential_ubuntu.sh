ebaed8b078fd430f0015a9a0459d20df5b12b7fb
Irina <irina@irina-Inspiron-N5110.(none)>
2013-01-21 17:05:51 +0200

    Fix script syntax

--- install_essential_ubuntu.sh
+++ install_essential_ubuntu.sh
@@ -1,15 +1,7 @@
 sudo apt-get update
-sudo apt-get install libgstreamer*
-sudo apt-get install libx264*
-sudo apt-get install libgst*
-sudo apt-get install gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly-*
-sudo apt-get install gstreamer0.10-ffmpeg
-
-
-sudo apt-get install libgtk*
-sudo apt-get install libz-dev
-sudo apt-get install liblzo2-dev
-sudo apt-get install libSDL*
-sudo apt-get install libboost-thread1.46-dev
-
-sudo apt-get install vlc
+sudo apt-get install -y libgstreamer* \
+libx264* libgst* \
+gstreamer0.10-plugins-good gstreamer0.10-plugins-bad \
+gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly-* \
+gstreamer0.10-ffmpeg \
+libgtk* libz-dev liblzo2-dev libSDL* libboost-thread1.46-dev vlc
