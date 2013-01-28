fa3cb4bb87bb5a5cd6bb05bf28541d680f29d122
Irina <irina@irina-Inspiron-N5110.(none)>
2013-01-28 19:38:32 +0200

    Add gstreamer sdk for android and env.sh for environment setup

---
+++ env.sh
@@ -0,0 +1,4 @@
+ROOT=$PWD
+export GSTREAMER_SDK_ROOT_ANDROID=$ROOT/external/gstreamer-android-sdk-2012-11/
+export JAVA_HOME=/shared/jdk1.6.0_26
+export PATH=/shared/android-ndk-r8d:$PATH
---
+++ external/gstreamer-android-sdk-2012-11/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/slomo/Projects/gstreamer/gst-sdk/cerbero/dist/android/lib"
+XML2_LIBS="-lxml2 -lz   -liconv -lm "
+XML2_INCLUDEDIR="-I/home/slomo/Projects/gstreamer/gst-sdk/cerbero/dist/android/include/libxml2"
+MODULE_VERSION="xml2-2.8.0"
+
