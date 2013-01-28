d4c7cbb94a67496f9f0cc328384652ec27fe4b0d
Irina <irina@irina-Inspiron-N5110.(none)>
2013-01-28 20:32:19 +0200

    Export IP address as argument

--- tests/pure_rtp_scripts_client_server/test_server_mpeg2.sh
+++ tests/pure_rtp_scripts_client_server/test_server_mpeg2.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+if [[ -z $1 ]]; then
+    echo "wrong parameter."
+    echo "$0 [destination ip port]"
+    exit -1
+fi
 AV=/dev/video0
 VIDEO_UDP_PORT=9978
 width=640
 height=480
 gst-launch-0.10 --gst-debug-level=0 v4l2src device=$AV ! video/x-raw-yuv,width=$width,height=$height ! \
 videorate ! ffmpegcolorspace ! mpeg2enc ! \
-udpsink host=192.168.43.1 port=$VIDEO_UDP_PORT sync=false
+udpsink host=$1 port=$VIDEO_UDP_PORT sync=false
