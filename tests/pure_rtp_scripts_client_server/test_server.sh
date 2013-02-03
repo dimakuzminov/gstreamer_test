5814aa8bd8bf133b0d1e6947fd07dd9c8c29d074
dmitry <dmitry@dmitry-ThinkPad-W530.(none)>
2013-02-03 14:06:44 +0200

    Change scripts to use address from command line

--- tests/pure_rtp_scripts_client_server/test_server.sh
+++ tests/pure_rtp_scripts_client_server/test_server.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+if [[ -z $1 ]]; then
+    echo "wrong parameter."
+    echo "$0 [destination ip port]"
+    exit -1
+fi
 AV=/dev/video0
 VIDEO_UDP_PORT=9978
 width=640
@@ -6,4 +11,4 @@ height=480
 gst-launch-0.10 --gst-debug-level=0 v4l2src device=$AV ! video/x-raw-yuv,width=$width,height=$height ! \
 videorate ! ffmpegcolorspace ! video/x-raw-yuv,width=$width,height=$height ! \
 x264enc tune=zerolatency byte-stream=true bitrate=3000 speed-preset=ultrafast ! rtph264pay name=pay0 pt=96 ! \
-udpsink host=127.0.0.1 port=$VIDEO_UDP_PORT sync=false
+udpsink host=$1 port=$VIDEO_UDP_PORT sync=false
--- tests/pure_rtp_scripts_client_server/test_server_h263.sh
+++ tests/pure_rtp_scripts_client_server/test_server_h263.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+if [[ -z $1 ]]; then
+    echo "wrong parameter."
+    echo "$0 [destination ip port]"
+    exit -1
+fi
 AV=/dev/video0
 VIDEO_UDP_PORT=9978
 width=704
@@ -6,4 +11,4 @@ height=576
 gst-launch-0.10 --gst-debug-level=1 v4l2src device=$AV ! \
 video/x-raw-yuv, framerate=30/1, width=640, height=480 ! videoscale ! videorate ! \
 video/x-raw-yuv,width=704,height=576,framerate=30/1 ! ffenc_h263 ! rtph263pay ! \
-udpsink host=127.0.0.1 port=$VIDEO_UDP_PORT sync=false
+udpsink host=$1 port=$VIDEO_UDP_PORT sync=false
